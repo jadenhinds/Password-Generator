b4d47cf8756ed209e7a599088fd348cf74ddf64b
jadenhinds <161650833+jadenhinds@users.noreply.github.com>
2024-08-13 00:29:04 -0400

    separating into new packages

--- src/PassGenerator.java
+++ src/PassGenerator.java
@@ -38,9 +38,7 @@ public class PassGenerator {
 
     static String newPassword() {
 
-        boolean[] mode = Settings.alphabetSettings();
-
-        String[] alphas = Settings.createAlphabet(mode[0], mode[1], mode[2], mode[3]).toArray(new String[0]);
+        String[] alphas = Settings.Alphabet();
 
         return PassGenerator.makePass(alphas, 7, 9);
     }
